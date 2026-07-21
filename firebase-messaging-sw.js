0e207f7c54a34cfb4f8538da438278c909117c64
mcgsandesh <maharashtrachagovinda@gmail.com>
2026-07-21 22:01:59 +0530

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+// Background Notification Receiver File
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+
+// Config background logic when needed
+self.addEventListener('push', function(event) {
+  // Logic for background notification trigger
+});
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mrdga/sw.js', { scope: '/mrdga/' })})}
